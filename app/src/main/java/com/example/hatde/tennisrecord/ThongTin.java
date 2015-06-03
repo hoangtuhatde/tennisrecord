7868c6b4b329fa8e11f9650e3b41a4dc33adcc53
hoangtuhatde <hoangtuhatde@gmail.com>
2015-06-04 04:41:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hatde/tennisrecord/ThongTin.java
@@ -0,0 +1,51 @@
+package com.example.hatde.tennisrecord;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ThongTin extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin);
+        Button btStartMatch = (Button)findViewById(R.id.btStartMatch_info);
+        btStartMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ThongTin.this, TranDau.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                ThongTin.this.startActivity(myIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_thong_tin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
