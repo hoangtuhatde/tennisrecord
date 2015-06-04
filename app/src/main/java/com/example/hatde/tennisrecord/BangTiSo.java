0ad119f23edf317f6e2506cac96578857b1c9cf1
shudanti <shudanti@gmail.com>
2015-06-04 19:44:40 +0700

    ngon

--- app/src/main/java/com/example/hatde/tennisrecord/BangTiSo.java
+++ app/src/main/java/com/example/hatde/tennisrecord/BangTiSo.java
@@ -6,12 +6,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by Saturn on 6/3/2015.
  */
-public class BangTiSo extends View {
+public class BangTiSo extends LinearLayout {
 
     int num_set;
     String player_1 = "A";
@@ -25,9 +27,13 @@ public class BangTiSo extends View {
 
     private int mWidth = 700;
     private int mHeight = 200;
+    public BangTiSo(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeViews(context);
+    }
     public BangTiSo(Context context, AttributeSet attrs) {
         super(context, attrs);
-
+        initializeViews(context);
 
 
         // Then allow overrides from XML
@@ -48,32 +54,47 @@ public class BangTiSo extends View {
     }
     private void commonSetup() {
 
+
         mPaint = new Paint();
         mPaint.setColor(line_color);
         // Scale the desired text size to match screen density
         mPaint.setTextSize(mFontSize * getResources().getDisplayMetrics().density);
         mPaint.setStrokeWidth(2f);
         setPadding(5, 5, 5, 5);
+
     }
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(mWidth, mHeight);
-    }
+    //@Override
+    //protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //setMeasuredDimension(mWidth, mHeight);
+
+    //}
     public BangTiSo(Context context) {
         super(context);
+        initializeViews(context);
         commonSetup();
+    }
+
+    private void initializeViews(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.matchboard, this);
+
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int top = getPaddingTop();
+        /*int top = getPaddingTop();
         int bot = mHeight - getPaddingBottom();
         int leftSide = (int) (mWidth*0.10f);
         int rightSide = (int) (mWidth*0.90f);
         Paint.Style oldStyle = mPaint.getStyle();
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(leftSide, top, rightSide, bot, mPaint);
-        mPaint.setStyle(oldStyle);
+        mPaint.setStyle(oldStyle);*/
+    }
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
     }
 }
